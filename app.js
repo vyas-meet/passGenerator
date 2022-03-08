01f872b2045193d7da5a8f8887f4c4212c7dbb6f
Meet Vyas <meet.vyas@somaiya.edu>
2022-03-08 20:40:40 +0530

    pass gen

---
+++ app.js
@@ -0,0 +1,24 @@
+const inputEl = document.querySelector("#lengthInput");
+const infoEl = document.querySelector(".info");
+const passContainers = document.querySelectorAll(".pw");
+
+const getRandomPassword = () => {
+    let pass = "";
+    let length = inputEl.value ? inputEl.value : 8;
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * (93 - 33 + 1)) + 33;
+        pass += String.fromCharCode(randomIndex)
+    }
+    return pass;
+}
+
+document.querySelector(".generate-btn").addEventListener("click", e => {
+    e.preventDefault();
+    if (inputEl.value > 25) {
+        infoEl.textContent = `Please keep the length below 25 characters.`
+    } else {
+        passContainers.forEach(el => {
+            el.textContent = getRandomPassword();
+        })
+    }
+})
